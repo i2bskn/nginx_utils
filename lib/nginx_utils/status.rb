5dc238244bfb10becb4df656f949b09096330f1a
i2bskn <i2bskn@gmail.com>
2013-05-21 12:23:59 +0900

    bug fix and add bin

--- lib/nginx_utils/status.rb
+++ lib/nginx_utils/status.rb
@@ -3,15 +3,18 @@
 module NginxUtils
   module Status
     class << self
-      def get(options={host: "localhost", port: 80, path: "/nginx_status"})
+      def get(options={})
+        host = options[:host] || "localhost"
+        port = options[:port] || 80
+        path = options[:path] || "/nginx_status"
         begin
-          req = Net::HTTP::Get.new(options[:path])
-          res = Net::HTTP.start(options[:host], options[:port]){|http| http.request(req)}
+          req = Net::HTTP::Get.new(path)
+          res = Net::HTTP.start(host, port){|http| http.request(req)}
           status = res.body.split("\n")
           server = status[2].split.map{|i| i.to_i}
           rww = status[3].split.select{|i| /^[0-9]+$/ =~ i}.map{|i| i.to_i}
           {
-            active_connection: status[0].split(":").last.gsub(/\s/, "").to_i,
+            active_connections: status[0].split(":").last.gsub(/\s/, "").to_i,
             accepts: server[0],
             handled: server[1],
             requests: server[2],
--- spec/nginx_utils/status_spec.rb
+++ spec/nginx_utils/status_spec.rb
@@ -7,7 +7,7 @@ describe NginxUtils do
     describe ".get" do
       it "should get status" do
         body = "Active connections: 1 \nserver accepts handled requests\n 4 5 51 \nReading: 1 Writing: 3 Waiting: 2 \n"
-        status = {active_connection: 1, accepts: 4, handled: 5, requests: 51, reading: 1, writing: 3, waiting: 2}
+        status = {active_connections: 1, accepts: 4, handled: 5, requests: 51, reading: 1, writing: 3, waiting: 2}
         response = double("http response mock", body: body)
         Net::HTTP.should_receive(:start).and_return(response)
         expect(NginxUtils::Status.get).to eq(status)
