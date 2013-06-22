bb06a7c7534db4e6aba404a38c64e66a24e9ab8e
i2bskn <i2bskn@gmail.com>
2013-06-23 05:51:51 +0900

    edit spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,10 @@ require "simplecov"
 require "coveralls"
 Coveralls.wear!
 
+# SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
 SimpleCov.start do
   add_filter "spec"
+  add_filter ".bundle"
 end
 
 require "nginx_utils"
